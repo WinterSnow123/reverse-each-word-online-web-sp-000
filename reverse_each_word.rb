0c27e584266865df83d077b93827b8f1c5caa15e
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-19 18:55:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   reversed = []
   string.split.collect do |word|
-    reversed[0] << "#{reserved[0]} #{word.reverse}"
+    reversed[0] = "#{reserved[0]} #{word.reverse}"
   end
 end
